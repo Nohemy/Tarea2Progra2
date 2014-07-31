542ab28abe00e2a8c72a5cc8bd961b7f97881802
Nohemy <nohemymiranda@unitec.edu>
2014-07-31 08:15:36 -0600

    Update PentagonoRegular.java

--- src/PentagonoRegular.java
+++ src/PentagonoRegular.java
@@ -6,14 +6,18 @@ PentagonoRegular (int lado){
 	this.lado=lado;
 	
 }
+//Obtenemos la cantidad del lado del pentagono
 int getLado()
 {
 	return lado;
 }
+//Obtenemos el perimetro del pentagono multiplicando el lado por 5
+//http://www.vitutor.net/2/1/4.html
 int getPerimetro()
 {
 	return lado*5;
 }
+//Devolvemos el valor final del lado de un pentagono
 void setLado(int lado){
 	this.lado=lado;
 }
